62b14b8f2d519bef0dedc50bddce428f3afabebb
Souvik93 <souvik.das@live.in>
2020-06-11 22:25:24 +0530

    1 file modified

--- index.js
+++ index.js
@@ -79,6 +79,30 @@ https://www.codementor.io/@niladrisekhardutta/how-to-call-apply-and-bind-in-java
 */
 
 
+function foo(a) {
+  this.a = a;
+}
+
+ var meObj = new foo(2);
+
+console.log('Hey There !! From New Binding Example');
+console.log(meObj.a);
+
+
+function foo(something) {
+  this.a = something;
+}
+
+
+var obj1 = {};
+
+var bar = foo.bind( obj1 );
+bar( 2 );
+console.log( obj1.a ); // 2
+
+var baz = new bar( 3 );
+console.log( obj1.a ); // 2 Since Hard Binding
+console.log( baz.a ); // 3 Since New Binding
 
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
